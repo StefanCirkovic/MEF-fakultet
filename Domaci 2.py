116717dbbafe00858703b6f00ca84080ced97934
StefanCirkovic <stefandjordje1508@gmail.com>
2024-10-24 17:21:21 +0200

    Add files via upload
    Predavanje 3; Stringovi; Domaci

---
+++ Domaci 2.py
@@ -0,0 +1,34 @@
+
+string = input("Unesite neki tekst: ")
+
+# 1. zadatak a)
+print(f"Ukupan broj znakova u stringu je {len(string)}")
+
+# 1. zadatak b)
+print(string * 10)
+
+# 1. zadatak c)
+print(string[0])
+
+# 1. zadatak d)
+print(string[:3])
+
+# 1. zadatak e)
+print(string[-3:])
+
+# 1. zadatak f)
+print(string[::-1])
+
+
+# 2. zadatak
+print('Didn\'t Hamlet say \"To be or not to be"')
+
+
+# 3. zadatak
+
+user_string_input = input("Unesite neki tekst: ")
+user_number_input = int(input("Koliko puta zelite da se vas tekst ponovi: "))
+
+print(user_string_input * user_number_input)
+
+
