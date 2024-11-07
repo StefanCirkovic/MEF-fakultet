234978d9f4d76287159dbfa0b5ad4d91308f1476
StefanCirkovic <stefandjordje1508@gmail.com>
2024-11-07 19:30:52 +0100

    Add files via upload
    Предавање 5 - For петља - домаћи

---
+++ Domaci 4.py
@@ -0,0 +1,116 @@
+# Domaci 4
+
+# 1. zadatak:
+
+
+for i in range(100):
+    print("Stefan Cirkovic")
+    
+# 2. zadatak:
+
+for i in range(40):
+    print("Stefan Cirkovic", end=" ")
+print()
+
+
+# 3. zadatak:
+
+for i in range(1, 101):
+    print(f"{i}. Stefan Cirkovic")
+
+# 4. zadatak:
+
+
+for i in range(1, 101):
+    print(f"{i}---{i*i}")
+
+
+# 5. zadatak:
+
+
+for i in range(8, 90, 3):
+    print(i)
+
+
+
+# dodatni zadaci:
+
+
+
+
+
+# 1.zadatak:
+    
+for i in range(100, 1, -2):
+    print(i)
+
+
+
+
+
+
+
+# 2.zadatak:
+
+for i in range(10):
+    print("A", end="")
+
+for i in range(7):
+    print("B", end="")
+
+for i in range(4):
+    print("CD", end="")
+
+print("E", end="")
+for i in range(6):
+    print("F", end="")
+
+print("G")
+
+
+
+
+
+
+
+# 3.zadatak:
+
+name = input("Unesite vase ime:")
+printed_amount = int(input("Unesite koliko puta zelite da se vase ime ponavlja: "))
+
+for i in range(printed_amount):
+    print(name)
+
+
+
+
+
+
+# 4.zadatak:
+
+for i in range(4):
+    print("*****************")
+
+
+
+
+
+
+# 5.zadatak:
+
+
+
+redovi = 4
+
+for i in range(1, redovi + 1):
+    print("*" * i)
+
+
+
+
+
+
+
+
+
+
